b6392657ac6e5da153cbb85a2bc7fd373ab9e3d6
Carlos Perez-Lopez <carlospzlz@gmail.com>
2018-03-03 22:22:44 +0000

    Added `is_palindrome` programming question.

---
+++ include/is_palindrome.h
@@ -0,0 +1,3 @@
+#include <string>
+
+bool IsPalindrome(const std::string& str);
---
+++ src/is_palindrome.cc
@@ -0,0 +1,13 @@
+#include "is_palindrome.h"
+
+bool IsPalindrome1(const std::string& str)
+{
+	for (size_t i = 0; i < str.length() / 2; ++i)
+	{
+		if (str[i] != str[str.length() - 1 - i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
