174d4e9fd91485198fdf1b8119a6686a9f5f2475
Carlos Perez-Lopez <carlospzlz@gmail.com>
2018-03-03 23:26:30 +0000

    Individual tests executable for each programming question.

--- tests/is_palindrome_test.cc
+++ tests/is_palindrome_test.cc
@@ -1,12 +1,23 @@
 #include "is_palindrome.h"
 #include "gtest/gtest.h"
 
-TEST(IsPalindromeTest, emptyStringIsPalindrome)
+TEST(IsPalindromeTest, EmptyStringIsPalindrome)
 {
 	EXPECT_TRUE(IsPalindrome1(""));
 }
 
-TEST(IsPalindromeTest, emptyStringIsPalindrome1)
+TEST(IsPalindromeTest, StringIsPalindrome)
 {
-	EXPECT_TRUE(IsPalindrome1(""));
+	EXPECT_TRUE(IsPalindrome1("asantadoglivedasadevilgodatnasa"));
+}
+
+TEST(IsPalindromeTest, StringIsNotPalindrome)
+{
+	EXPECT_FALSE(IsPalindrome1("helloworld"));
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
